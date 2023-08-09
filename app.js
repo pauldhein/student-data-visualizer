dc9dc217f40364e627761527c28c7db77c8ff87c
Paul Hein <paul.d.hein@gmail.com>
2023-08-08 17:14:05 -0700

    Setup express for http serving

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 5000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log("Server successfully running on port 5000");
+});
